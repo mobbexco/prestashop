7bf86098bcfec7bbf573540cfa3e4eeed12c4b84
jlmobbex <javier.lopez@mobbex.com>
2021-05-09 17:04:50 -0300

    Add powershell compress support to Build.sh

--- build.sh
+++ build.sh
@@ -1,37 +1,14 @@
 #!/bin/sh
 
 VER="2.1.4"
-CUR_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
-
-# Create 1.6 version
-# PRESTAV="1.6"
-
-# cp $PRESTAV/mobbex.php mobbex/mobbex.php
-# zip mobbex.$VER.ps-$PRESTAV.zip -r mobbex
-# rm mobbex/mobbex.php
-# PRESTAV="1.6"
-
-# cp $PRESTAV/mobbex.php mobbex/mobbex.php
-# zip mobbex.$VER.ps-$PRESTAV.$CUR_TIME.zip -r mobbex
-# zip mobbex.$VER.ps-$PRESTAV.zip -r mobbex
-# rm mobbex/mobbex.php
-
-# Create 1.7 version
-# PRESTAV="1.7"
-
-# cp $PRESTAV/mobbex.php mobbex/mobbex.php
-# zip mobbex.$VER.ps-$PRESTAV.$CUR_TIME.zip -r mobbex
-# zip mobbex.$VER.ps-$PRESTAV.zip -r mobbex
-# rm mobbex/mobbex.php
 
 # Unified Version
 PRESTAV="1.6-1.7"
 
 if type 7z > /dev/null; then
-    7z a -tzip "mobbex.$VER.ps-$PRESTAV.$CUR_TIME.zip" mobbex
     7z a -tzip "mobbex.$VER.ps-$PRESTAV.zip" mobbex
-else
-    zip mobbex.$VER.ps-$PRESTAV.$CUR_TIME.zip -r mobbex
+elif type zip > /dev/null; then
     zip mobbex.$VER.ps-$PRESTAV.zip -r mobbex
+elif type powershell > /dev/null; then
+    powershell Compress-Archive mobbex mobbex.$VER.ps-$PRESTAV.zip
 fi
-
