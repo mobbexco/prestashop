941005a1b6527362b7b52b11aa703c8a247451b5
jlmobbex <javier.lopez@mobbex.com>
2021-06-09 11:14:01 -0300

    Remove powershell compress support

--- build.sh
+++ build.sh
@@ -9,6 +9,4 @@ if type 7z > /dev/null; then
     7z a -tzip "mobbex.$VER.ps-$PRESTAV.zip" mobbex
 elif type zip > /dev/null; then
     zip mobbex.$VER.ps-$PRESTAV.zip -r mobbex
-elif type powershell > /dev/null; then
-    powershell Compress-Archive mobbex mobbex.$VER.ps-$PRESTAV.zip
 fi
