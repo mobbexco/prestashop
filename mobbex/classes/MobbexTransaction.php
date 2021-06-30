de21f9f988ab16b44f04aa96fe02d506d08f416e
jlmobbex <javier.lopez@mobbex.com>
2021-06-30 11:32:55 -0300

    Update version

--- mobbex/classes/MobbexTransaction.php
+++ mobbex/classes/MobbexTransaction.php
@@ -5,7 +5,7 @@
  * Transaction model
  *
  * @author  Mobbex Co <admin@mobbex.com>
- * @version 2.2.0
+ * @version 2.2.1
  * @see     PaymentModuleCore
  */
 class MobbexTransaction extends ObjectModel
--- mobbex/controllers/front/notification.php
+++ mobbex/controllers/front/notification.php
@@ -5,7 +5,7 @@
  * Main file of the module
  *
  * @author  Mobbex Co <admin@mobbex.com>
- * @version 2.2.0
+ * @version 2.2.1
  * @see     PaymentModuleCore
  */
 
