1d60ffc8fa99ad06e097b1ae90d0c2c1564cba39
jlmobbex <javier.lopez@mobbex.com>
2021-07-02 19:25:43 -0300

    Update version

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Módulo del Gateway Mobbex para Prestashop.
 
 ### Versión Actual
 
-- 2.2.1
+- 2.2.2
 
 ### Versiones de Prestashop Soportadas  
 
--- mobbex/controllers/front/wallet.php
+++ mobbex/controllers/front/wallet.php
@@ -5,7 +5,7 @@
  * Main file of the module
  *
  * @author  Mobbex Co <admin@mobbex.com>
- * @version 2.2.1
+ * @version 2.2.2
  * @see     PaymentModuleCore
  */
 
