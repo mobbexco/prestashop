c18f24a333a1e8704bb660a0ba530bb8cc38d29f
jlmobbex <javier.lopez@mobbex.com>
2021-08-03 00:06:40 -0300

    Update version

--- mobbex/classes/MobbexTransaction.php
+++ mobbex/classes/MobbexTransaction.php
@@ -5,7 +5,7 @@
  * Transaction model
  *
  * @author  Mobbex Co <admin@mobbex.com>
- * @version 2.3.0
+ * @version 2.3.1
  * @see     PaymentModuleCore
  */
 class MobbexTransaction extends ObjectModel
