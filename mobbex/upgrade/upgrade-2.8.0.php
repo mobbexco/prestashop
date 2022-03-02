05e204f16ce26f323f8511ee5099e200414a4c9c
jlmobbex <javier.lopez@mobbex.com>
2022-03-02 19:20:44 -0300

    Add translations and upgrade script

---
+++ mobbex/upgrade/upgrade-2.8.0.php
@@ -0,0 +1,35 @@
+<?php
+
+defined('_PS_VERSION_') || exit;
+
+function upgrade_module_2_8_0(Mobbex $module)
+{
+    $configs = [
+        'color'      => trim((string) \Configuration::get('MOBBEX_PLANS_TEXT_COLOR')),
+        'background' => trim((string) \Configuration::get('MOBBEX_PLANS_BACKGROUND')),
+        'padding'    => trim((string) \Configuration::get('MOBBEX_PLANS_PADDING')),
+        'font-size'  => trim((string) \Configuration::get('MOBBEX_PLANS_FONT_SIZE')),
+    ];
+
+    // Migrate previus plan configs to new option
+    \Configuration::updateValue(\MobbexHelper::K_PLANS_STYLES,
+'.mbbxWidgetOpenBtn {
+    width: fit-content;
+    min-height: 40px;
+    border-radius: 6px;
+    padding: ' . ($configs['padding'] ?: '8px 18px') . '; /* arriba/abajo, izquierda/derecha */
+    font-size: ' . ($configs['font-size'] ?: '16px') . ';
+    color: ' . ($configs['color'] ?: '#6f00ff') . '; 
+    background: ' . ($configs['background'] ?: '#ffffff') . ';
+    border: 2px solid #6f00ff; /* grosor, estilo de linea, color */
+    cursor: pointer;
+    /*box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, .2);*/
+}
+
+.mbbxWidgetOpenBtn:hover {
+    color: #ffffff;
+    background-color: #6f00ff;
+}');
+
+    return $module->_createTable() && $module->unregisterHooks() && $module->registerHooks() && $module->addExtensionHooks();
+}
