50baa99ceef6d6d5b7469bd00c33fab8b9e95fda
Luca Longoni <lucalong54@gmail.com>
2022-08-19 15:15:53 -0300

    fixes-1.01

--- mobbex/classes/MobbexHelper.php
+++ mobbex/classes/MobbexHelper.php
@@ -518,8 +518,13 @@ class MobbexHelper
         $custom_dni = MobbexCustomFields::getCustomField($customer_id, 'customer', 'dni');
         if(!$custom_dni){
             return $custom_dni;
-        }else if(!empty(DB::getInstance()->executeS("SHOW COLUMNS FROM $table LIKE '$dniColumn'")) || !empty(DB::getInstance()->executeS("SHOW COLUMNS FROM $table LIKE '$identifier'"))){
-            return DB::getInstance()->getValue("SELECT $dniColumn FROM $table WHERE $identifier='$customer_id'");
+        }else if(!empty($dniColumn)){
+            if(!empty(DB::getInstance()->executeS("SHOW COLUMNS FROM $table LIKE '$dniColumn'")) || !empty(DB::getInstance()->executeS("SHOW COLUMNS FROM $table LIKE '$identifier'"))){
+                return DB::getInstance()->getValue("SELECT $dniColumn FROM $table WHERE $identifier='$customer_id'");
+            }
+            else{
+                return;
+            }
         }else{
             return;
         }
--- mobbex/views/templates/hooks/dnifield.tpl
+++ mobbex/views/templates/hooks/dnifield.tpl
@@ -1,15 +1,15 @@
 {literal}
 <script>
-  var billingDni = document.querySelector('#billing_dni');
+  var customerDni = document.querySelector('#customer_dni');
 
   document.forms['account-creation_form'].onsubmit = function () {
-    if (billingDni.value == "") {
+    if (customerDni.value == "") {
       return false;
     }
   }
 </script>
 {/literal}
 <div class="required form-group">
-  <label for="billing_dni">DNI <sup>*</sup></label>
-  <input type="text" class="is_required validate form-control" data-validate="billing_dni" id="billing_dni" name="billing_dni" value="{$last_dni}">
+  <label for="customer_dni">DNI <sup>*</sup></label>
+  <input type="text" class="is_required validate form-control" data-validate="customer_dni" id="customer_dni" name="customer_dni" value="{$last_dni}">
 </div>
