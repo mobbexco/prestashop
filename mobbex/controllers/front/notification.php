3a708922c9e440ea064a63c16c7209df2ccacfc2
EMunozMobbex <e.munoz.chedel@mobbex.com>
2022-12-19 17:22:02 -0300

    fix: CMMRC-160
    Fixed the behavior of financial discounts in orders.

--- mobbex/controllers/front/notification.php
+++ mobbex/controllers/front/notification.php
@@ -136,13 +136,16 @@ class MobbexNotificationModuleFrontController extends ModuleFrontController
                 
             } else {
                 // Update cart total
-                $this->orderUpdate->updateCartTotal($cartId, $data['total']);
+                $cartRule = $this->orderUpdate->updateCartTotal($cartId, $data['total']);
 
                 // Create and validate Order
                 $order = \Mobbex\PS\Checkout\Models\Helper::createOrder($cartId, $data['order_status'], $data['source_name'], $this->module);
 
                 if ($order)
                     $this->orderUpdate->updateOrderPayment($order, $data);
+
+                if(is_object($cartRule))
+                    $cartRule->delete();
             }
         }
 
