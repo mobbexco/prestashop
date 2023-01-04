188b70230ccdc91febccc631336012a807081204
jlmobbex <javier.lopez@mobbex.com>
2023-01-04 12:50:45 -0300

    Update version

--- mobbex/upgrade/upgrade-3.4.1.php
+++ mobbex/upgrade/upgrade-3.4.1.php
@@ -11,7 +11,7 @@ defined('_PS_VERSION_') || exit;
  * 
  * @return bool Upgrade result.
  */
-function upgrade_module_3_4_0($module) {
+function upgrade_module_3_4_1($module) {
     return $module->installer->createTables()
         && $module->installer->createStates($module->config->orderStatuses)
         && $module->installer->createCostProduct()
