26916bc31d788525a236fccf4acf9bed328bae94
jlmobbex <javier.lopez@mobbex.com>
2023-03-10 14:09:20 -0300

    Update version

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-VER="3.5.1"
+VER="3.5.2"
 
 # Unified Version
 PRESTAV="1.6-1.7"
+
+# Install dependencies
+cd mobbex
+composer update
+cd ..
 
 if type 7z > /dev/null; then
     7z a -tzip "mobbex.$VER.ps-$PRESTAV.zip" mobbex
 elif type zip > /dev/null; then
     zip mobbex.$VER.ps-$PRESTAV.zip -r mobbex
 fi
+
+# Remove dep files
+rm -r mobbex/vendor mobbex/composer.lock
--- mobbex/upgrade/upgrade-3.5.2.php
+++ mobbex/upgrade/upgrade-3.5.2.php
@@ -11,7 +11,7 @@ defined('_PS_VERSION_') || exit;
  * 
  * @return bool Upgrade result.
  */
-function upgrade_module_3_5_1($module) {
+function upgrade_module_3_5_2($module) {
     return $module->installer->createTables()
         && $module->installer->createStates($module->config->orderStatuses)
         && $module->installer->createCostProduct()
